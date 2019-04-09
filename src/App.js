17e0ae764b4239fc6bf6a42fa41abf88cd4bc2bb
Israel-Laguan <israellaguan@gmail.com>
2019-04-08 23:54:38 -0500

    Added Functional Components and Hooks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import { YText } from "yjs/types/YText";
 import { WebsocketProvider } from "yjs/provider/websocket";
-import { TextareaBinding } from "yjs/bindings/textarea";
-import logo from "./logo.svg";
-import "./App.css";
+import { QuillBinding } from 'yjs/bindings/quill.js'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
-class App extends Component {
-  componentDidMount() {
+
+const App = () => {
+  let quillRef=null;
+  let reactQuillRef=null;
+
+  React.useEffect(() => {
+    attachQuillRefs()
     const provider = new WebsocketProvider("wss://yjs-react-example.herokuapp.com/");// change to 'ws://localhost:3000'
     const ydocument = provider.get("textarea");
     const type = ydocument.define("textarea", YText);
-    const binding = new TextareaBinding(type, this.refs.share);
+    new QuillBinding(type, quillRef);
+  }, [])
+
+  const attachQuillRefs = () => {
+    if (typeof reactQuillRef.getEditor !== 'function') return;
+    quillRef = reactQuillRef.getEditor();
   }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div>
-            <textarea ref="share" />
-          </div>
-        </header>
+  
+  const insertText = () => {
+    var range = quillRef.getSelection();
+    let position = range ? range.index : 0;
+    quillRef.insertText(position, 'Hello, World! ')
+  }
+  return (
+    <div>
+        <ReactQuill 
+          ref={(el) => { reactQuillRef = el }}
+          theme={'snow'} />
+        <button onClick={insertText}>Insert Text</button>
       </div>
-    );
-  }
+  )
 }
 
 export default App;
